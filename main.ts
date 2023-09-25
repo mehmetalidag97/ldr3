a0c3d593e385d9d9ba63a782ebac2271215a2e4c
Mehmet Ali <78815495+mehmetalidag97@users.noreply.github.com>
2023-09-25 10:07:22 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+namespace ldr {
+    /**
+     * Read the LDR value (0-1023).
+     */
+    //% blldr.readValue()ock
+    export function readValue(): number {
+        // Implement your code here to read the LDR value from your module
+        // You can use pins and analogRead to read the value
+        return 0; // Placeholder, replace with actual value
+    }
+} basic.forever(function () {
+
+})
