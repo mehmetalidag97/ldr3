7841d9bb74bc2a6a71181c87e0b4fbf1acaec231
Mehmet Ali <78815495+mehmetalidag97@users.noreply.github.com>
2023-09-25 10:15:04 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,10 @@ namespace ldr {
     /**
      * Read the LDR value (0-1023).
      */
-    //% blldr.readValue()ock
+    //% block
     export function readValue(): number {
+        // Implement your code here to read the LDR value from your module
+        // You can use pins and analogRead to read the value
         return 0; // Placeholder, replace with actual value
     }
-} basic.forever(function () {
-
-})
+}
