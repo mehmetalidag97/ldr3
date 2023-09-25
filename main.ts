0a570fe5e7e5638592c725b0331cd516a1d4e874
Mehmet Ali <78815495+mehmetalidag97@users.noreply.github.com>
2023-09-25 10:34:57 +0300

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,6 @@ namespace ldr {
      */
     //% block
     //% subcategory="LDR"
-
     export function ReadLdrValue(): number {
         return 0; 
     }
@@ -18,5 +17,17 @@ namespace potentiometer {
     //% subcategory="POT"
     export function ReadPotValue(): number {
         return 0;
+    }
+}
+
+namespace ldrtest {
+    /**
+     * Read the LDR value (0-1023) from pin 27 (assuming it's an analog pin).
+     */
+    //% block
+    export function readLDRValue(): number {
+        // Read the LDR value from pin 27 (assuming it's an analog pin)
+        let ldrValue = pins.analogReadPin(27);
+        return ldrValue;
     }
 }
